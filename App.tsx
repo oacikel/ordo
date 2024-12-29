acf725add191bc309ce65e79140350305ee56021
Oğulcan Açıkel <ogulcan.acikel@gmail.com>
2024-12-29 22:22:50 +0300

    fix: update tsconfig.json and App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import 'react-native-reanimated'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
@@ -7,7 +9,7 @@ export default function App() {
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
