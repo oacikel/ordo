61bbdae9df8535fce7934d3f064428c662102fbc
Oğulcan Açıkel <ogulcan.acikel@gmail.com>
2025-01-09 22:36:12 +0300

    bugfix: handle open close status toggle correctly in the app bar

--- src/screens/FileDetailsScreen.tsx
+++ src/screens/FileDetailsScreen.tsx
@@ -35,26 +35,6 @@ export default function  FileDetails({ route, navigation }: FileDetailsProps) {
   const [editable, setEditable] = useState<boolean>(isEditMode) // In the original request there is no need to change edit mode, but it can be added later
   const [fileStatus, setFileStatus] = useState<FileStatusType>('Open')
   const updateFile = useFileStore((state) => state.updateFile)
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      header: () => <CustomAppBar file={file} onToggleStatus={function (): void {
-        switch (files.find((file) => file.id === fileId)?.status) {
-          case 'Open':
-            if (file) {
-              updateFile(file.id, { status: 'Closed' })
-              setFileStatus('Closed')
-            }
-            break
-          case 'Closed':
-            if (file) {
-              updateFile(file.id, { status: 'Open' })
-              setFileStatus('Open')
-            }
-            break
-        }
-      } } />,
-    });
-  }, [navigation, fileStatus, fileId, file]);
   
   useEffect(() => {
     if (fileId) {
@@ -118,21 +98,38 @@ export default function  FileDetails({ route, navigation }: FileDetailsProps) {
   }
 
   return (
-        <Tab.Navigator initialRouteName={getInitialRouteName()}>
-        <Tab.Screen
-          name={t('informationTab')}
-          children={() => (
-          <TabA 
-            file={file} 
-            setFile={setFile} 
-            editable={editable} 
+    
+    <View style={{ flex: 1 }}>
+    <CustomAppBar
+      mandatoryInput={file.mandatoryInput}
+      status={file.status}
+      dateInput={file.dateInput}
+      onToggleStatus={function (): void {
+        const currentFile = files.find((file) => file.id === fileId);
+        if (currentFile) {
+          const newStatus = currentFile.status === 'Open' ? 'Closed' : 'Open';
+          updateFile(currentFile.id, { status: newStatus });
+          setFileStatus(newStatus);
+          setFile({ ...currentFile, status: newStatus });
+        }
+      }}
+    />
+    <Tab.Navigator initialRouteName={getInitialRouteName()}>
+      <Tab.Screen
+        name={t('informationTab')}
+        children={() => (
+          <TabA
+            file={file}
+            setFile={setFile}
+            editable={editable}
             handleSave={handleSave}
           />
-          )}
-          options={{ lazy: false }}
-        />
-        <Tab.Screen name={t('tabBScreenName')} component={TabB}/>
-        <Tab.Screen name={t('tabCScreenName')} component={TabC}/>
-        </Tab.Navigator>
+        )}
+        options={{ lazy: false }}
+      />
+      <Tab.Screen name={t('tabBScreenName')} component={TabB} />
+      <Tab.Screen name={t('tabCScreenName')} component={TabC} />
+    </Tab.Navigator>
+  </View>
   )
 }
